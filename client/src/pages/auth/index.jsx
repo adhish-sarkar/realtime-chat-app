37b3cf589da8c5638c24399cde7241ef40775c4d
Adhish Sarkar <sarkaradhish131@gmail.com>
2024-09-11 15:56:00 +0530

    auth ui complete

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,36 +1,119 @@
-import React from 'react'
-import Background from '@/assets/login2.png'
-import Victory from "@/assets/victory.svg"
-import { Tabs, TabsList,TabsContent,TabsTrigger } from '@/components/ui/tabs'
+import React, { useState } from "react";
+import Background from "@/assets/login2.png";
+import Victory from "@/assets/victory.svg";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 export const Auth = () => {
-  return (
-    <div className='h-[100vh] w-[100vw] flex items-center justify-center'>
-        <div className='h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2'>
-                <div className='flex flex-col gap-10 items-center justify-center'>
-                    <div className='flex items-center justify-center flex-col'>
-                        <div className='flex items-center justify-center'>
-                            <h1 className='text-5xl font-bold md:text-6xl'>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleLogin = async () => {};
+
+    const handleSignup = async () => {};
+
+    return (
+        <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+            <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+                <div className="flex flex-col gap-10 items-center justify-center">
+                    <div className="flex items-center justify-center flex-col">
+                        <div className="flex items-center justify-center">
+                            <h1 className="text-5xl font-bold md:text-6xl">
                                 Welcome
                             </h1>
-                            <img src={Victory} alt="victory image" className='h-[100px]' />
-                            <p className='font-medium text-center'>
-                                Fill in the details to get started with the best Chat app!
-                            </p>
+                            <img
+                                src={Victory}
+                                alt="victory image"
+                                className="h-[100px]"
+                            />
                         </div>
+                            <p className="font-medium text-center">
+                                Fill in the details to get started with the best
+                                Chat app!
+                            </p>
+                            </div>
                         <div className="flex item-center justify-center w-full">
-                            <Tabs>
-                                <TabsList>
-                                    <TabsTrigger value='login'>Login</TabsTrigger>
-                                    <TabsTrigger value='signup'>SignUp</TabsTrigger>
+                            <Tabs className="w-3/4">
+                                <TabsList className="bg-transparent rounded-none w-full">
+                                    <TabsTrigger
+                                        value="login"
+                                        className="data-[state:active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                                    >
+                                        Login
+                                    </TabsTrigger>
+                                    <TabsTrigger
+                                        value="signup"
+                                        className="data-[state:active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                                    >
+                                        SignUp
+                                    </TabsTrigger>
                                 </TabsList>
-                                <TabsContent></TabsContent>
-                                <TabsContent></TabsContent>
+                                <TabsContent
+                                    className="flex flex-col gap-5 mt-10"
+                                    value="login"
+                                >
+                                    <Input
+                                        placeholder="Email"
+                                        type="email"
+                                        value={email}
+                                        className="rounded-full p-6"
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
+                                    <Input
+                                        placeholder="Password"
+                                        type="password"
+                                        value={password}
+                                        className="rounded-full p-6"
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
+                                    <Button className="rounded-full p-6" onclick={handleLogin}>Log in</Button>
+                                </TabsContent>
+                                <TabsContent
+                                    className="flex flex-col gap-5 mt-10"
+                                    value="signup"
+                                >
+                                    <Input
+                                        placeholder="Email"
+                                        type="email"
+                                        value={email}
+                                        className="rounded-full p-6"
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
+                                    <Input
+                                        placeholder="Password"
+                                        type="password"
+                                        value={password}
+                                        className="rounded-full p-6"
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
+                                    <Input
+                                        placeholder="ConfirmPassword"
+                                        type="password"
+                                        value={confirmPassword}
+                                        className="rounded-full p-6"
+                                        onChange={(e) =>
+                                            setConfirmPassword(e.target.value)
+                                        }
+                                    />
+                                    <Button className="rounded-full p-6" onclick={handleSignup}>Sign up</Button>
+                                </TabsContent>
                             </Tabs>
                         </div>
-                    </div>
                 </div>
+                    <div className=" hidden xl:flex justify-center items-center">
+                        <img src={Background} alt="Background login" className="h-[700px]" />
+                    </div>
+            </div>
         </div>
-    </div>
-  )
-}
-export default Auth
+    );
+};
+export default Auth;
